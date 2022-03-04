be53dcaa8cdb9426d4153358c4ec5530bd64722a
sky <anvanc37@gmail.com>
2022-03-04 17:59:58 -0500

    implement MapKit

--- Africa/Africa/Helper/VideoPlayerHelper.swift
+++ Africa/Africa/Helper/VideoPlayerHelper.swift
@@ -14,7 +14,8 @@ func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
     if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
         videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
         videoPlayer?.play()
+        return videoPlayer!
     }
     
-    return videoPlayer!
+    return AVPlayer()
 }
